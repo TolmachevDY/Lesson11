afc8025a6631b65c49595cd53347dd106898e358
Tolmachev DY <Tolmachev DY@DESKTOP-5RJHKAS>
2022-11-21 21:25:43 +0300

    Добавьте файлы проекта.

---
+++ Task1/Line.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    struct Line
+    {
+        double k;
+        double b;         
+        
+        public Line(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+
+        public string Root()
+        {
+            if (k == 0 && b == 0)
+                return "Решений бесконечно много"; 
+            if (k == 0)
+                return "Ошибка. Деление на ноль";            
+            if (k == 0 && b != 0)
+                return "Решений нет";
+            else
+            {
+                double r = -b / k;
+                return Convert.ToString(r);
+            }
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Решение линейного уравнения kx+b=0\nk=");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b=");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Line line = new Line(k, b);
+            Console.WriteLine("х={0}",line.Root());
+            Console.ReadKey();
+        }
+    }
+}
